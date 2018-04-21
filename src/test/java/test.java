f698ea5742653ed358a82bfa05dbc8881b9012c7
IUgly <15223166166@163.com>
2018-04-21 10:38:13 +0800

    init

--- src/test/java/test.java
+++ src/test/java/test.java
@@ -11,8 +11,6 @@ import java.util.Map;
 
 public class test {
     public static void main(String[] args) {
-       String url = "https://www.sojson.com/open/api/weather/json.shtml?city=%E9%87%8D%E5%BA%86";
-        com.alibaba.fastjson.JSONObject json = WechatUtil.getInfo(url);
-        System.out.println(json);
+       
     }
 }
